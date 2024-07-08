026f73d9fb9ee8744780e0c7ff6f6553691d0fc9
Chukwudimma Oradiegwu <oradiegwuchukwudimma@gmail.com>
2024-07-08 11:14:35 -0700

    commit this

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,43 +1,5 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * 
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
-
-/**
- * print_string - prints a string using _putchar
- * @str: The string to print
- */
-void print_string(const char *str)
-{
-    while (*str)
-    {
-        _putchar(*str++);
-    }
-}
-
-/**
- * print_hex - prints a hex value using _putchar
- * @num: The number to print in hex
- */
-void print_hex(unsigned char num)
-{
-    char hex_digits[] = "0123456789abcdef";
-
-    _putchar('0');
-    _putchar('x');
-    _putchar(hex_digits[num / 16]);
-    _putchar(hex_digits[num % 16]);
-}
+#include <stdio.h>
 
 /**
  * simple_print_buffer - prints buffer in hexa
@@ -48,23 +10,23 @@ void print_hex(unsigned char num)
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-    unsigned int i;
+        unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
+        i = 0;
+        while (i < size)
         {
-            _putchar(' ');
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
         }
-        if (!(i % 10) && i)
-        {
-            _putchar('\n');
-        }
-        print_hex(buffer[i]);
-        i++;
-    }
-    _putchar('\n');
+        printf("\n");
 }
 
 /**
@@ -78,7 +40,7 @@ int main(void)
 
     simple_print_buffer(buffer, 98);
     _memset(buffer, 0x01, 95);
-    print_string("-------------------------------------------------\n");
+    printf("-------------------------------------------------\n");
     simple_print_buffer(buffer, 98);    
     return (0);
 }
--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte.
- * @s: pointer to the memory area to be filled
- * @b: the constant byte to fill the memory area with
- * @n: the number of bytes to be filled
- * 
- * Return: pointer to the filled memory area
+ * _memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
