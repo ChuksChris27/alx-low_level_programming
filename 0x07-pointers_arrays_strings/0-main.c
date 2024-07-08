eb1229b50c808b35debda231e1a84de3cbcb0547
Chukwudimma Oradiegwu <oradiegwuchukwudimma@gmail.com>
2024-07-08 10:57:59 -0700

    commit this

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,5 +1,43 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+
+/**
+ * print_string - prints a string using _putchar
+ * @str: The string to print
+ */
+void print_string(const char *str)
+{
+    while (*str)
+    {
+        _putchar(*str++);
+    }
+}
+
+/**
+ * print_hex - prints a hex value using _putchar
+ * @num: The number to print in hex
+ */
+void print_hex(unsigned char num)
+{
+    char hex_digits[] = "0123456789abcdef";
+
+    _putchar('0');
+    _putchar('x');
+    _putchar(hex_digits[num / 16]);
+    _putchar(hex_digits[num % 16]);
+}
 
 /**
  * simple_print_buffer - prints buffer in hexa
@@ -10,23 +48,23 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-        unsigned int i;
+    unsigned int i;
 
-        i = 0;
-        while (i < size)
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
         {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
+            _putchar(' ');
         }
-        printf("\n");
+        if (!(i % 10) && i)
+        {
+            _putchar('\n');
+        }
+        print_hex(buffer[i]);
+        i++;
+    }
+    _putchar('\n');
 }
 
 /**
@@ -40,7 +78,7 @@ int main(void)
 
     simple_print_buffer(buffer, 98);
     _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
+    print_string("-------------------------------------------------\n");
     simple_print_buffer(buffer, 98);    
     return (0);
 }
