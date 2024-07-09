3e2afdafed85c8a349464ca837bfcb00aa85c05b
Chukwudimma Oradiegwu <oradiegwuchukwudimma@gmail.com>
2024-07-08 18:59:35 -0700

    commit this

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stddef.h>
 
+void set_string(char **s, char *to);
 unsigned int _strspn(char *s, char *accept);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
