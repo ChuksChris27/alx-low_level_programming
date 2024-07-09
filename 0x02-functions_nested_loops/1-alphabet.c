6865c870063dd28b655caf0bd6ade7f37d218a5e
Chukwudimma Oradiegwu <oradiegwuchukwudimma@gmail.com>
2024-07-09 15:00:34 -0700

    commit this

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints alphabets 'a - z'
- *
- * FUNCTIONALITY *
- *
- * 1. Use a for loop starting with 'a' and ending with 'z'.
- * 2. Within the loop, call _putchar to print each character.
- * 3. After the loop, call _putchar to print a new line.
- *
- * Time Complexity - O(1)
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line
  */
 void print_alphabet(void)
 {
-	for (int c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+
+    _putchar('\n');
 }
